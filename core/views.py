b73628528195f11fb5bec56538993e03cf786bfd
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2025-01-20 16:08:34 +0700

    create frontpage template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
+# Flowchart View
+# views.py -> *.html ->  urls.py
+
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
