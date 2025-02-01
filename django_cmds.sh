246669d730b9eb326c12286329a7204451181aec
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2025-02-01 16:00:38 +0700

    initiate item app

---
+++ django_cmds.sh
@@ -0,0 +1,15 @@
+# Run server
+python manage.py runserver
+
+# Make App section
+python manage.py startipp "<YOUR-APP-NAME>"
+
+# Make Migrations
+python manage.py makemigrations
+
+# Apply Migration
+python manage.py migrate
+
+# Create Super User
+python manage.py createsuperuser
+> go to [http://<url>/admin/] to manage user
