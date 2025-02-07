4d00e009debb3206ca3781aeb2c98b064b9c9b3a
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2025-02-07 19:05:05 +0700

    create item detail and related items

--- djangoo_marketplace/urls.py
+++ djangoo_marketplace/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # Image Import
 from django.conf import settings
@@ -10,6 +10,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
