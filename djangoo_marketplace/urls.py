0927138c86cb329874ae5504fbd11211d6cd8ca0
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2025-03-20 22:36:57 +0700

    modify 🚀 - urls for core and items

--- djangoo_marketplace/urls.py
+++ djangoo_marketplace/urls.py
@@ -6,11 +6,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # Import
-from core.views import index, contact
+# from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
+    # path('contact/', contact, name='contact'),
+
+    # Admin Site
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
