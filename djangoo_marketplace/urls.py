43a271ff793d684a6efc03600d23e44ca08d262c
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2025-05-15 20:25:14 +0700

    feat - create conversation urls

--- djangoo_marketplace/urls.py
+++ djangoo_marketplace/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('items/', include('item.urls')),
     # path('contact/', contact, name='contact'),
     path('dashboard/', include('dashboard.urls')),
-
+    path('inbox/', include('conversation.urls')),
     # Admin Site
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
